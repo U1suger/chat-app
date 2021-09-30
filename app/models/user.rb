39a4e51b48986c5f1b250bfad77303029774c8a3
U1suger <u1.suger1201@gmail.com>
2021-09-30 11:41:30 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :room_users
+  has_many :rooms, through: :room_users
   validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
